b5f9141add122c0433e07e9cd45dfc18fe267d46
Mr.NGUYEN <mrnguyen@MrNGUYENs-MacBook-Pro.local>
2014-11-25 17:23:54 +0700

    Initial Commit

---
+++ EssentialDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EssentialDemo
+//
+//  Created by Gau Uni on 11/25/14.
+//  Copyright (c) 2014 iOS_Team_CO2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EssentialDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EssentialDemo
+//
+//  Created by Gau Uni on 11/25/14.
+//  Copyright (c) 2014 iOS_Team_CO2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
